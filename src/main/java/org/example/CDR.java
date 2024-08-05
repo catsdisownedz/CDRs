8b51eef07dd21481856312695401ca244e971261
zeina <catsdisownedz@gmail.com>
2024-08-05 10:36:32 +0300

    XML FINALLY WORKED!

--- src/main/java/org/example/CDR.java
+++ src/main/java/org/example/CDR.java
@@ -1,8 +1,9 @@
 package org.example;
+
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-//@XmlRootElement(name = "cdr")
+@XmlRootElement(name = "cdr")
 public class CDR {
     private String anum;
     private String bnum;
@@ -10,7 +11,11 @@ public class CDR {
     private double usage;
     private String startDateTime;
 
-    public CDR(String anum, String bnum, String serviceType, float usage, String startDateTime) {
+    // Default no-arg constructor required for JAXB
+    public CDR() {
+    }
+
+    public CDR(String anum, String bnum, String serviceType, double usage, String startDateTime) {
         this.anum = anum;
         this.bnum = bnum;
         this.serviceType = serviceType;
@@ -18,18 +23,48 @@ public class CDR {
         this.startDateTime = startDateTime;
     }
 
-    public String getAnum() { return anum; }
-    public void setAnum(String anum) { this.anum = anum; }
+    @XmlElement(name = "anum")
+    public String getAnum() {
+        return anum;
+    }
 
-    public String getBnum() { return bnum; }
-    public void setBnum(String bnum) { this.bnum = bnum; }
+    public void setAnum(String anum) {
+        this.anum = anum;
+    }
 
-    public String getServiceType() { return serviceType; }
-    public void setServiceType(String serviceType) { this.serviceType = serviceType; }
+    @XmlElement(name = "bnum")
+    public String getBnum() {
+        return bnum;
+    }
 
-    public double getUsage() { return usage; }
-    public void setUsage(float usage) { this.usage = usage; }
+    public void setBnum(String bnum) {
+        this.bnum = bnum;
+    }
 
-    public String getStartDateTime() { return startDateTime; }
-    public void setStartDateTime(String startDateTime) { this.startDateTime = startDateTime; }
+    @XmlElement(name = "serviceType")
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    @XmlElement(name = "usage")
+    public double getUsage() {
+        return usage;
+    }
+
+    public void setUsage(double usage) {
+        this.usage = usage;
+    }
+
+    @XmlElement(name = "startDateTime")
+    public String getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(String startDateTime) {
+        this.startDateTime = startDateTime;
+    }
 }
